3fbff57617d62361642e404923eab7fee203800c
Svenja Schramm <svenja@Svenjas-iMac.fritz.box>
2019-04-12 11:16:10 +0200

    190412

--- src/app/heroes/heroes.component.ts
+++ src/app/heroes/heroes.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Hero } from '../hero';
 
 @Component({
   selector: 'app-heroes',
@@ -6,11 +7,14 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./heroes.component.css']
 })
 export class HeroesComponent implements OnInit {
-  hero = 'Windstorm';
+  hero: Hero={ 
+    id: 1, 
+    name: 'Windshit'
+  };
   constructor() { }
 
   ngOnInit() {
-    
+    console.log('huhu');
   }
 
 }
